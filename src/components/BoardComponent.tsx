674583492c0520b1d1606a9b017d0b9972e04a84
Kiper02 <permakovkirill@gmail.com>
2024-07-09 20:53:02 +0500

    baseStyles-startCube

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+
+function BoardComponent() {
+    return ( 
+        <div className='board'>
+            <div className="cell white"></div>
+            <div className="cell black"></div>
+            <div className="cell white"></div>
+            <div className="cell black"></div>
+        </div>
+     );
+}
+
+export default BoardComponent;
