e54d32792cd3951e72786f8964c448ebf9e07019
Kiper02 <permakovkirill@gmail.com>
2024-07-10 17:19:28 +0500

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
 import { Board } from './models/Board';
+import { Player } from './models/Player';
+import { Colors } from './models/Colors';
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 
 function App() {
   const [board, setBoard] = useState(new Board())
-
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null >(null)
+  
   useEffect(() => {
     restart()
+    setCurrentPlayer(whitePlayer)
   }, [])
 
 
@@ -18,11 +26,29 @@ function App() {
     setBoard(newBoard)
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
   return (
    <div className='app'>
+    <Timer 
+      restart={restart}
+      currentPlayer={currentPlayer}
+    />
       <BoardComponent 
         board={board}
         setBoard={setBoard}
+        currentPlayer = {currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+
+      <LostFigures 
+        title={"Черные фигуры"} 
+        figures={board.lostBlackFigures}
+      />
+      <LostFigures 
+        title={"Белые фигуры"} 
+        figures={board.lostWhiteFigures}
       />
    </div>
   );
